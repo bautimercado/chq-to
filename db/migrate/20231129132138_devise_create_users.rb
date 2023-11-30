34b59dddfaeb9e033e5e932cca664e354f3a7159
Bautista Mercado <bautista.mercado184770@alumnos.info.unlp.edu.ar>
2023-11-29 21:38:16 -0300

    Add user show and destroy

--- db/migrate/20231129132138_devise_create_users.rb
+++ db/migrate/20231129132138_devise_create_users.rb
@@ -4,7 +4,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :username, null: false
       t.string :email, null: false
       t.string :encrypted_password, null: false
 
@@ -39,7 +38,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
 
     add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :username, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_132138) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_235331) do
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
     t.string "reset_password_token"
@@ -20,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_132138) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
